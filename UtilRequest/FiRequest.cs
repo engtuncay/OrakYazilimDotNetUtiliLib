cd8b80a2593345b580a87a9f51bab9462a27eb23
Tuncay <tunc270@gmail.com>
2025-01-25 21:18:31 +0300

    up

---
+++ UtilRequest/FiRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace OrakYazilimLib.UtilRequest
+{
+    /// <summary>
+    /// Request yapılırken gönderilen temel bilgiler : kullanıcı vs.
+    /// </summary>
+    public class FiRequest
+    {
+        public string firTxProfile { get; set; }
+        public string firTxUser { get; set; }
+        public string firTxPass { get; set; }
+
+        
+    }
+}
