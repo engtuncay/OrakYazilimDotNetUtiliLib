25e2e6f137c2b88fafe63bbe6aa550b438eff023
TuncayHp <tunc270@gmail.com>
2025-01-27 00:06:37 +0300

    up

---
+++ Util/core/FiConsole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace OrakYazilimLib.Util.core
+{
+    public class FiConsole
+    {
+        public static void printAllMembers(Object obj)
+        {
+            foreach(PropertyDescriptor descriptor in TypeDescriptor.GetProperties(obj))
+            {
+                string name=descriptor.Name;
+                object value=descriptor.GetValue(obj);
+                Console.WriteLine($"{name}={value}");
+            }
+        }
+    }
+}
--- UtilRequest/FiRequest.cs
+++ UtilRequest/FiRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace OrakYazilimLib.UtilRequest
+﻿using OrakYazilimLib.Util.core;
+
+namespace OrakYazilimLib.UtilRequest
 {
     /// <summary>
     /// Request yapılırken gönderilen temel bilgiler : kullanıcı vs.
@@ -8,7 +10,6 @@
         public string firTxProfile { get; set; }
         public string firTxUser { get; set; }
         public string firTxPass { get; set; }
-
-        
+        public FiKeybean firFkbParams { get; set; }
     }
 }
