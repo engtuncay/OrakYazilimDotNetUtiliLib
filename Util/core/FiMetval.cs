87d1a90224601d58784da418be605f36cbd6f43e
TuncayHp <tunc270@gmail.com>
2025-01-26 12:41:51 +0300

    up

---
+++ Util/core/FiMetval.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrakYazilimLib.Util.core
+{
+    public class FiMetval
+    {
+        /**
+         * TxCode (TxKodu)
+        */
+        public string txKey { get; set; }
+
+        public string txValue { get; set; }
+
+        /**
+         * LnCode (LnKodu)
+         * <p>
+         * Key Meta Karşılık Gelen Integer Kod varsa
+         */
+        public int lnKey { get; set; }
+
+        /**
+         * Açıklama (Description) gibi düşünebiliriz
+         */
+        public string txLabel { get; set; }
+
+        public string txType { get; set; }
+
+        public FiMetval() {}
+
+        public FiMetval(string txValue)
+        {
+            this.txValue = txValue;
+        }
+
+
+    }
+}
