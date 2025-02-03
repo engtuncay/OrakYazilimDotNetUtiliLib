67b3fbd917b7b48b5fc9131dda29d8dffce45dba
TuncayHp <tunc270@gmail.com>
2025-02-03 21:09:57 +0300

    up

--- Util/config/IFiConfigManager.cs
+++ Util/config/IFiConfigManager.cs
@@ -1,9 +1,12 @@
 ﻿using OrakYazilimLib.Util.core;
+using System.Collections.Generic;
 
 namespace OrakYazilimLib.Util.config
 {
     public interface IFiConfigManager
     {
+        Dictionary<string, string?> mapConnString { get; set; }
+
         string GetConnString(string profile);
     }
 }
